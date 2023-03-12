f785b502a98a7bad866ef267389ed21f52391889
Pawel Liberski <panliberski@outlook.com>
2023-03-12 21:18:39 +0000

    Add project files.

---
+++ MyMauiApp/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace MyMauiApp;
+
+/// <summary>
+/// This is the code-behind for the page. 
+/// In this file, you define the logic for the various event handlers and other actions that are triggered by the controls on the page. 
+/// The example code implements a handler for the Clicked event for the button on the page. 
+/// The code simply increments a counter variable and displays the result in a label on the page. 
+/// The Semantic service provided as part of the MAUI Essentials library supports accessibility. 
+/// The static Announce method of the SemanticScreenReader class specifies the text announced by a screen reader 
+/// when the user selects the button:
+/// </summary>
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times, I urge you to stop it immediately!";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
---
+++ MyMauiApp/MauiProgram.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+
+///Each native platform has a different starting point that creates and initializes the application. 
+///You can find this code under the Platforms folder in the project. This code is platform-specific, 
+///but at the end it calls the CreateMauiApp method of the static MauiProgramclass. 
+///You use the CreateMauiApp method to configure the application by creating an app builder object. 
+///At a minimum, you need to specify which class describes your application. 
+///You do this with the UseMauiApp generic method of the app builder object; 
+///the type parameter specifies the application class. The app builder also provides methods for tasks such as registering fonts, 
+///configuring services for dependency injection, registering custom handlers for controls, and more. 
+///The following code shows an example of using the app builder to register a font:
+
+
+
+namespace MyMauiApp;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
